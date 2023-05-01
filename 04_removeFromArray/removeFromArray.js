355f4cb0e8d3a1327d35f0dd31c5f8e46a5e9d74
OctoKing-dev <51433961+OctoKing-dev@users.noreply.github.com>
2023-04-30 23:14:26 -0400

    Solve 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...values) {
+  const results = [];
+  for (let i=0; i < array.length; i++) {
+    const value = array[i];
+    if (!values.includes(value)) {
+      results.push(value);
+    }
+  }
 
+  return results;
 };
 
 // Do not edit below this line

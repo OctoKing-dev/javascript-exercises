06b9a79c9ed0e3b7fb53990fedc5f94b7fd98b0e
OctoKing-dev <51433961+OctoKing-dev@users.noreply.github.com>
2023-04-30 23:35:19 -0400

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+  // Must be a valid integer
+  if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+  // Neither number may be negative
+  if (a < 0 || b < 0) return "ERROR";
 
+  // Support a being greater than b, or the inverse
+  let min = (a <= b) ? a : b;
+  let max = (a >= b) ? a : b;
+
+  let sum = 0;
+
+  for (let i=min; i <= max; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line

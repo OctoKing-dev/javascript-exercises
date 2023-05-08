d13e845d1a2ee74f64f9d0cc54057a133e208210
OctoKing-dev <51433961+OctoKing-dev@users.noreply.github.com>
2023-05-08 16:20:59 -0400

    Solve 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  if (n < 0) return 'OOPS';
+  if (n === 0) return 0;
 
+  let a = 0;
+  let b = 1;
+  for (let i = 1; i < n; i++) {
+    let newB = a + b;
+    a = b;
+    b = newB;
+  }
+
+  return b;
 };
 
 // Do not edit below this line

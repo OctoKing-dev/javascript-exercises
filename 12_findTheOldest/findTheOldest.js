aac03dbd7228f6a2667866b21847701d500d51e8
OctoKing-dev <51433961+OctoKing-dev@users.noreply.github.com>
2023-05-08 16:54:18 -0400

    Solve 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  const currentYear = (new Date).getFullYear();
+  return people.reduce((oldest, person) => {
+    const oldestAge = (oldest['yearOfDeath'] ?? currentYear) - oldest.yearOfBirth;
+    const thisAge = (person['yearOfDeath'] ?? currentYear) - person.yearOfBirth;
 
+    return (thisAge > oldestAge) ? person : oldest;
+  });
 };
 
 // Do not edit below this line

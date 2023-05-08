bbb977ad00596da812b8ac0ebcd54b70ddd50fc0
OctoKing-dev <51433961+OctoKing-dev@users.noreply.github.com>
2023-05-08 16:00:00 -0400

    Solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const replaced = string.toUpperCase().replace(/[^A-Z]/g, "");
+  const reversed = replaced.split('').reverse().join('');
 
+  return replaced === reversed;
 };
 
 // Do not edit below this line
